6cb5eda6639674c11563017f330f569019b889ad
Juan Carlos Rivera <juan.rivera@mimacom.com>
2020-01-05 18:45:08 +0100

    32. Step 18 Part 2 - Internationalization for RESTful Services

--- src/main/java/com/jc/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/jc/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -3,12 +3,10 @@ package com.jc.rest.webservices.restfulwebservices.controller;
 import com.jc.rest.webservices.restfulwebservices.data.HelloWorldBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
@@ -31,10 +29,9 @@ public class HelloWorldController {
 
     //hello world bean internationalized
     @GetMapping(path = "/hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language",
-                                                             required = false) Locale locale) {
+    public String helloWorldInternationalized() {
 
-        return messageSource.getMessage("good-morning.message", null, locale);
+        return messageSource.getMessage("good-morning.message", null, LocaleContextHolder.getLocale());
     }
 
     @GetMapping(path = "/hello-world-path-variable/{name}")
