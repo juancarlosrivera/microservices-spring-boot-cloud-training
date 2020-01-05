00183791d9b79728e24d7934905fb5c69a311bcd
Juan Carlos Rivera <juan.rivera@mimacom.com>
2020-01-05 21:51:52 +0100

    44. Step 30 - Creating User Entity and some test data

--- src/main/java/com/jc/rest/webservices/restfulwebservices/model/User.java
+++ src/main/java/com/jc/rest/webservices/restfulwebservices/model/User.java
@@ -3,13 +3,17 @@ package com.jc.rest.webservices.restfulwebservices.model;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @ApiModel(description = "This is my user model")
+@Entity
 public class User {
 
+    @Id
     private int id;
     @Size(min = 2,
           message = "Name should have at least 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (1, sysdate(), 'JC');
+insert into user values (2, sysdate(), 'Mary');
+insert into user values (3, sysdate(), 'Sofia');
