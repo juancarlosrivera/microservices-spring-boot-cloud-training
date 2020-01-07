fe06d21e8fedc6ce2509146a7d1ed178f9045177
Juan Carlos Rivera <juan.rivera@mimacom.com>
2020-01-07 12:37:01 +0100

    45. Step 31 - Updating GET methods on User Resource to use JPA

---
+++ src/main/java/com/jc/rest/webservices/restfulwebservices/controller/UserJPAResource.java
@@ -0,0 +1,70 @@
+package com.jc.rest.webservices.restfulwebservices.controller;
+
+import com.jc.rest.webservices.restfulwebservices.dao.UserDaoService;
+import com.jc.rest.webservices.restfulwebservices.exceptions.UserNotFoundException;
+import com.jc.rest.webservices.restfulwebservices.model.User;
+import com.jc.rest.webservices.restfulwebservices.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserJPAResource {
+
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //get detail a specific user
+    @GetMapping("/jpa/users/{userId}")
+    public Resource<User> getUser(@PathVariable int userId) {
+
+        Optional<User> user = userRepository.findById(userId);
+        if (!user.isPresent()) {
+            throw new UserNotFoundException("Id" + userId);
+        }
+
+        Resource<User> resource = new Resource<User>(user.get());
+        ControllerLinkBuilder linkTo = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users"));
+
+        return resource;
+    }
+
+    //retrieve all users
+    @GetMapping("/jpa/users")
+    public List<User> retrieveAllUsers() {
+
+        return userRepository.findAll();
+    }
+
+    //Response with 201 Created and location header reponse with the URI to get the new user created
+    @PostMapping("/jpa/users")
+    public ResponseEntity.BodyBuilder createUser(@Valid @RequestBody User user) {
+
+        User savedUser = userRepository.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location);
+    }
+
+    @DeleteMapping("/jpa/users/{userId}")
+    public void deleteUser(@PathVariable int userId) {
+
+        userRepository.deleteById(userId);
+    }
+}
---
+++ src/main/java/com/jc/rest/webservices/restfulwebservices/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jc.rest.webservices.restfulwebservices.repository;
+
+import com.jc.rest.webservices.restfulwebservices.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
